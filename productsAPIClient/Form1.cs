9b960c32689305eb02ac6653261fd7e93ae483e7
Aidan Batchelor <abatchelor@aq-fes.com>
2021-03-29 15:25:55 +0100

    Add project files.

---
+++ productsAPIClient/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace productsAPIClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdSend_Click(object sender, EventArgs e)
+        {
+            RestClient rClient = new RestClient();
+            rClient.endPoint = txtUrl.Text;
+            txtResponse.Text = string.Empty;
+            debugOutput("Rest client created");
+            string strResponse = string.Empty;
+            strResponse = rClient.makeRequest(txtAqKeyName.Text.ToString(),txtAqKeyValue.Text.ToString());
+            deserializeJson(strResponse);
+        }
+
+        private void deserializeJson(string strJson)
+        {
+            try
+            {
+                var jsonObject = JsonConvert.DeserializeObject<dynamic>(strJson);
+                debugOutput(jsonObject.ToString());
+            }
+            catch(Exception ex)
+            {
+                debugOutput("Deserialization problem" + ex.Message.ToString());
+            }
+        }
+
+        private void debugOutput(string strDebugText)
+        {
+            System.Diagnostics.Debug.Write(strDebugText + Environment.NewLine);
+            txtResponse.Text += strDebugText + Environment.NewLine;
+        }
+
+        private void cmdMfrBillingInfo_Click(object sender, EventArgs e)
+        {
+            txtUrl.Text = "https://api.aq-fes.com/products-api/billing/manufacturers";
+        }
+
+        private void cmdMfrProducts_Click(object sender, EventArgs e)
+        {
+            txtUrl.Text = "https://api.aq-fes.com/products-api/manufacturers/" + txtMfrId.Text + "/products";
+        }
+
+        private void cmdProduct_Click(object sender, EventArgs e)
+        {
+            txtUrl.Text = "https://api.aq-fes.com/products-api/products/" + txtProdId.Text;
+        }
+    }
+}
---
+++ productsAPIClient/RestClient.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Collections.Generic;
+using System.Text;
+
+namespace productsAPIClient
+{
+    public enum httpVerb
+    {
+        GET,
+        POST,
+        PUT,
+        DELETE
+    }
+
+    class RestClient
+    {
+        public string endPoint { get; set; }
+        public httpVerb httpMethod { get; set; }
+        public string userName { get; set; }
+        public string userPassword { get; set; }
+        public RestClient()
+        {
+            endPoint = string.Empty;
+            httpMethod = httpVerb.GET;
+            
+        }
+
+        public string makeRequest(string username, string password)
+        {
+            string strResponseValue = string.Empty;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(endPoint);
+            request.Headers.Set(username, password);
+            request.Method = httpMethod.ToString();
+            HttpWebResponse response = null;
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+                //process reponse
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    if(responseStream != null)
+                    {
+                        using(StreamReader reader = new StreamReader(responseStream))
+                        {
+                            strResponseValue = reader.ReadToEnd();
+                        }//end of stream reader
+                    }
+                }//end of using responsestream
+            }
+            catch(Exception ex)
+            {
+                strResponseValue = "{\"errorMessages\":[\"" + ex.Message.ToString() + "\"],\"errors\":{}}";
+            }
+            finally
+            {
+                if(response != null)
+                {
+                    ((IDisposable)response).Dispose();
+                }
+            }
+
+            return strResponseValue;
+        }
+    }
+}
